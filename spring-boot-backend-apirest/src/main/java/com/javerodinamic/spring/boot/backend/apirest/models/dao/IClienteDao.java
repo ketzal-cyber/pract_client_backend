2123b3015c9b4e195fd4e688e13cf2c69d5346a1
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-01-05 01:12:06 -0600

    Interface ClienteDao para operaciones CRUD

---
+++ spring-boot-backend-apirest/src/main/java/com/javerodinamic/spring/boot/backend/apirest/models/dao/IClienteDao.java
@@ -0,0 +1,9 @@
+package com.javerodinamic.spring.boot.backend.apirest.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.javerodinamic.spring.boot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
+
+}
