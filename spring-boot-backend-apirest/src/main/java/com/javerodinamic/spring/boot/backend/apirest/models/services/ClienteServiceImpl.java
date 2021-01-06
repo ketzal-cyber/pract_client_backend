37f852cfacbf65e1279164caaf0a68e0aa2bb392
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-01-05 18:13:21 -0600

    implementar metodos save, update, delete

--- spring-boot-backend-apirest/src/main/java/com/javerodinamic/spring/boot/backend/apirest/models/services/ClienteServiceImpl.java
+++ spring-boot-backend-apirest/src/main/java/com/javerodinamic/spring/boot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -4,6 +4,7 @@
  * Los metodos del CRUDRepository ya son transaccionales se ** podria omitir
  * Se sobreescrie en el service para tener control y hcerlo mas explicita
  * Este clase se guardara en el contenedor de spring y posterior se puede inyectar en el controlador y asi lo podemos utiliar
+ * implementar metodos save, delete, findById
  */
 
 package com.javerodinamic.spring.boot.backend.apirest.models.services;
@@ -30,6 +31,29 @@ public class ClienteServiceImpl implements IClienteService {
 	public List<Cliente> findAll() {
 
 		return (List<Cliente>) clienteDao.findAll();
+	}
+
+// se prueba el metodo orElse para regrese un null en caso de no encontrar el id
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
+
+
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		// save regresa el  entity guardado 
+		return clienteDao.save(cliente);
+	}
+
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		// 
+		clienteDao.deleteById(id);
 	}
 
 }
--- spring-boot-backend-apirest/src/main/java/com/javerodinamic/spring/boot/backend/apirest/models/services/IClienteService.java
+++ spring-boot-backend-apirest/src/main/java/com/javerodinamic/spring/boot/backend/apirest/models/services/IClienteService.java
@@ -1,5 +1,6 @@
 /*
  * En esta interface le dare un contrato de implementación los metodos del CRUD
+ * siguente paso declarar metodos para crud findyid, save, delete
  */
 package com.javerodinamic.spring.boot.backend.apirest.models.services;
 
@@ -10,6 +11,16 @@ import com.javerodinamic.spring.boot.backend.apirest.models.entity.Cliente;
 
 public interface IClienteService {
 	
+	//metodo para listar los clientes
 	public List<Cliente> findAll();
+	
+	// metodo para buscar por id
+	public Cliente findById(Long id);
+	
+	//metodo para guardar un cliente
+	public Cliente save(Cliente cliente);
+	
+	//metodo para eliminar
+	public void delete(Long id);
 
 }
