bf75a06e6676ce7af3c092f547d4c256d992a4d5
ketzal-cyber <angelhernandez_1m368@hotmail.com>
2021-01-09 23:37:51 -0600

    capturar error en el metodo delete

--- spring-boot-backend-apirest/src/main/java/com/javerodinamic/spring/boot/backend/apirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/javerodinamic/spring/boot/backend/apirest/controllers/ClienteRestController.java
@@ -134,9 +134,20 @@ public class ClienteRestController {
 	
 	
 	@DeleteMapping("/clientes/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable Long id) {
-		clienteService.delete(id);
+	// 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			clienteService.delete(id);
+		}catch(DataAccessException e) {
+			response.put("Mensaje", "Error: al eliminar en la DB");
+			response.put("Mensaje", e.getMessage().concat(" : ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String , Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("Mensaje", "El cliente ha sido eliminado con exito");
+		return  new ResponseEntity<Map<String , Object>>(response, HttpStatus.OK);
 	}
 
 }
